afe0454d4d80eaf58cf9bde316940b9634f02667
David Jurado <davidblanco103@gmail.com>
2015-09-14 18:25:17 -0500

    Función onchange en menú de rutas
    Se eliminó el botón para mostrar cada ruta, ahora solo seleccionando la
    ruta se muestra y se agrego el mapa de todas las rutas para no tener que
    refrescar.

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ var marker;
 
 window.onload = function(){
 
-	document.getElementById("boton3").onclick =  showruta;
+	document.getElementById("ruta").onchange =  showruta;
 	x = document.getElementById("ruta");
 	};
 
@@ -22,45 +22,6 @@ function showruta()
 	var frame = document.getElementById("frame");
 	frame.setAttribute("src", x.value+".html");
 }
-
-function initestaciones(){	
-	var marker = new google.maps.Marker({
-	    position: {lat: 7.1376718, lng: -73.118959},
-	    map: map,
-	    title: 'UIS',
-	    icon: icon_estacion
-  		});
-
-	var marker = new google.maps.Marker({
-	    position: {lat: 7.0876912, lng: -73.1364687},
-	    map: map,
-	    title: 'terminal!',
-    	icon: icon_estacion
-	  });
-
-	var marker = new google.maps.Marker({
-	    position: {lat: 7.1247492, lng: -73.117962},
-	    map: map,
-	    title: 'niños',
-    	icon: icon_estacion
-	  });
-
-	var marker = new google.maps.Marker({
-	    position: {lat: 7.0998583, lng: -73.1131649},
-	    map: map,
-	    title: 'laSalle',
-    	icon: icon_estacion
-	  });
-	marker.setMap(map);
-
-}
-
-
-function deleteestaciones(){
-alert("no sé como eliminar estaciones");
-
-}
-
 
 
   

7a5f2cfbba9fe0904aa585c934f7553a88fc913e
anmalvar <andres-106@hotmail.com>
2015-09-25 18:55:06 -0500

    crear el Log
    para generar el log se usa ajax se hace una consulta sql por medio de php y los resultados van al html, luego con el boton descargar txt se captura la info del log y se descarga en un .txt

--- Log/log.js
+++ Log/log.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    document.getElementById('boton-txt').addEventListener('click', function () { 
+    document.getElementById('btnLog').addEventListener('click', function () { 
     var datos = obtenerDatos();
     descargarArchivo(generarTexto(datos), 'Log.txt');}, false);
     };
@@ -25,37 +25,19 @@ function descargarArchivo(contenidoEnBlob, nombreArchivo) {
 
 //Función de ayuda: reúne los datos a exportar en un solo objeto
 function obtenerDatos() {
-    // aca necesitamos el query que nos trae de la BD los datos de estacion ide bus y hora.
-    
     return {
-        estacion: "estacion1",
-        bus: "bus1",
-        fecha: "123"
+        resultado: document.getElementById('resultado').innerHTML
     };
 };
+
 
 
 //Genera un objeto Blob con los datos en un archivo TXT
 function generarTexto(datos) {
     var texto = [];
     texto.push('LOG:\n');
-
+    texto.push(datos.resultado);
     texto.push('\n');
-    texto.push("FECHA");
-    texto.push('\t');
-    texto.push("BUS");
-    texto.push('\t');
-    texto.push("ESTACION");
-    texto.push('\t');
-//FOR...
-    
-    texto.push('\n');
-    texto.push(datos.fecha);
-    texto.push('\t');
-    texto.push(datos.bus);
-    texto.push('\t');
-    texto.push(datos.estacion);
-    texto.push('\t');
     //El contructor de Blob requiere un Array en el primer parámetro
     //así que no es necesario usar toString. el segundo parámetro
     //es el tipo MIME del archivo
